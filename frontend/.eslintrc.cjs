5e77e237a63284b9889498d207acef376ba9c4d4
HungSun LIM <majac6@gmail.com>
2024-04-21 21:57:38 +0900

    pkg : 잘못된 prettier 설정 수정

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,30 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    browser: true, es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  overrides: [{
+    env: {
+      node: true,
+    }, files: [".eslintrc.{js,cjs}"], parserOptions: {
+      sourceType: "script",
     },
-  ],
+  }],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: "latest", sourceType: "module",
   },
   plugins: ["@typescript-eslint", "react", "prettier"],
   rules: {
